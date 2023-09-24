7512006b9be26e980393170a7428869309517a82
Obied <obiedullah@outlook.com>
2023-09-24 03:12:59 -0400

    fixed link

--- client/src/apis/apiConfig.js
+++ client/src/apis/apiConfig.js
@@ -1,4 +1,4 @@
-const apiBaseUrl = "https://obied-ullah-portfollio.onrender.com";
+const apiBaseUrl = "https://obied-ullah-portfolio.onrender.com";
 // const apiBaseUrl = ""
 
 export const contact = `${apiBaseUrl}/contact`;
